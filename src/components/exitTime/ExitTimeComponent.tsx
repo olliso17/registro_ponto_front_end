0ba7dc7aa7334579f7b7ab4b8cb6d9c30cba0fa8
patricia <patriciasilva>
2024-05-27 16:20:55 -0300

    update all exit time

---
+++ src/components/exitTime/ExitTimeComponent.tsx
@@ -0,0 +1,20 @@
+import FormComponent from "../form/FormComponent"
+import { TimerStatus } from "../hooks/time"
+import Top from "../top/Top"
+import classes from "./ExitTimeComponent.module.css"
+
+type Props = {
+    userData: any
+    time: any
+    status: TimerStatus
+}
+const ExitTimeComponent = ({ userData, time, status }: Props) => {
+
+    return (
+        <div className={classes.container}>
+            <Top time={time} userData={userData} status={status} />
+            <FormComponent userData={userData} />
+        </div>
+    )
+}
+export default ExitTimeComponent
--- src/routes/ExitTime.tsx
+++ src/routes/ExitTime.tsx
@@ -1,12 +1,27 @@
-import React from 'react'
-import FormComponent from '../components/form/FormComponent'
 
-const ExitTime = ()=>{
-    return(
-        <div>
-            <Top time={time} userData={userData} status={status}/>
+import { useParams } from 'react-router-dom';
+import ExitTimeComponent from '../components/exitTime/ExitTimeComponent';
+import useUserData from '../components/hooks/getEmployeeId';
+import useTimer from '../components/hooks/time';
+import classes from "./ExitTime.module.css";
 
-            <FormComponent name="Hora de Saída"/>
+
+const ExitTime = () => {
+    const { id } = useParams<{ id: string }>();
+    const { time, status } = useTimer();
+    const { userData, error, loading } = useUserData(id);
+
+    if (loading) {
+        return <div>Carregando...</div>;
+    }
+
+    if (error) {
+        return <div>{error}</div>;
+    }
+
+    return (
+        <div className={classes.entryTime}>
+            <ExitTimeComponent time={time} status={status} userData={userData} />
         </div>
     )
 }

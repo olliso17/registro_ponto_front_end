d7d364db0d2b5f9a615c6dd121d0ed3c6a208d06
patricia <patriciasilva>
2024-05-27 14:34:49 -0300

    update front_end

--- src/components/home/HomeComponent.tsx
+++ src/components/home/HomeComponent.tsx
@@ -4,6 +4,8 @@ import ButtonIcon from "../button/ButtonIcon";
 import ButtonMenu from "../button/ButtonMenu";
 import FormComponent from "../form/FormComponent";
 import TimerComponent from "../time/TimeComponent";
+import classes from "./HomeComponent.module.css"
+
 type Props = {
     userData: any
 }
@@ -12,9 +14,12 @@ const HomeComponent = ({ userData }: Props) => {
 
     return (
         <div>
-            <div>
-                <ButtonMenu name="Entry Time" router="/entry_time" />
-                <ButtonMenu name="Exite Time" router="/exit_time" />
+            <div className={classes.menu}>
+                <div>
+                    <ButtonMenu name="Entry Time" router="/entry_time" />
+                    <ButtonMenu name="Exite Time" router="/exit_time" />
+                </div>
+
                 <ButtonIcon children={<FaPowerOff className="icon" />} />
 
             </div>

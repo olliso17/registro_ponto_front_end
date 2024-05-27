17e47f1851a10c0a33fbd990f72ad2bd3dd8db12
olliso17 <olli.so17@gmail.com>
2024-05-27 20:41:43 -0300

    updated all

--- src/components/button/ButtonIcon.tsx
+++ src/components/button/ButtonIcon.tsx
@@ -2,12 +2,13 @@ import classes from "./ButtonIcon.module.css"
 
 type Props = {
     children: JSX.Element
+    onClick:()=>void
 }
 
 
-const ButtonIcon = ({ children }: Props) => {
+const ButtonIcon = ({ children, onClick }: Props) => {
     return (
-        <button className={classes.button}>
+        <button className={classes.button} onClick={onClick}>
             {children}
         </button>
     )
--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -2,15 +2,17 @@ import { FaPowerOff } from "react-icons/fa"
 import ButtonIcon from "../button/ButtonIcon"
 import ButtonMenu from "../button/ButtonMenu"
 import classes from "./Menu.module.css"
+import useAuth from "../hooks/useAuth"
 
 type Props = {
     router1: string
     router2: string
-    name1:string
-    name2:string
+    name1: string
+    name2: string
 }
 
-const Menu = ({router1, router2, name1, name2}:Props) => {
+const Menu = ({ router1, router2, name1, name2 }: Props) => {
+    const { logout } = useAuth();
     return (
         <div className={classes.menu}>
             <div>
@@ -18,7 +20,7 @@ const Menu = ({router1, router2, name1, name2}:Props) => {
                 <ButtonMenu name={name2} router={router2} />
             </div>
 
-            <ButtonIcon children={<FaPowerOff className="icon" />} />
+            <ButtonIcon children={<FaPowerOff className="icon" />}  onClick={logout}/>
 
         </div>
     )
